92ab6642eb34b97ac36d8846ee24d5f37388c7e0
Cameron Pollard-Grunill <CameronLP01@outlook.com>
2025-04-29 12:58:45 +0930

    Added GameGrid constructor

--- Connect4 BotApp/Connect4 BotApp/Program.cs
+++ Connect4 BotApp/Connect4 BotApp/Program.cs
@@ -10,8 +10,21 @@ public class Node
 
 // Contains the game board, and helping functions such as display
 public class GameGrid
-{ 
+{
+    // 2D array representing the game board and pieces
+    public string[,] gameGrid;
 
+    public GameGrid(int colCount, int rowCount)
+    {
+        gameGrid = new string[colCount, rowCount];
+        for (int c = 0; c < colCount; c++)
+        {
+            for (int r = 0; r < rowCount; r++)
+            {
+                gameGrid[c, r] = " ";
+            }
+        }
+    }
 }
 
 
