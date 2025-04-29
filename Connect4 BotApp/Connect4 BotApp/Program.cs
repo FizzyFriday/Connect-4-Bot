2b9db4a3c7ab8b8e024da49a2d6be0fa12f273be
Cameron Pollard-Grunill <CameronLP01@outlook.com>
2025-04-29 13:04:36 +0930

    Added Main function for Bot

--- Connect4 BotApp/Connect4 BotApp/Program.cs
+++ Connect4 BotApp/Connect4 BotApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 
 // Represents the nodes of the tree
@@ -12,16 +13,16 @@ public class Node
 public class GameGrid
 {
     // 2D array representing the game board and pieces
-    public string[,] gameGrid;
+    public string[,] grid;
 
     public GameGrid(int colCount, int rowCount)
     {
-        gameGrid = new string[colCount, rowCount];
+        grid = new string[colCount, rowCount];
         for (int c = 0; c < colCount; c++)
         {
             for (int r = 0; r < rowCount; r++)
             {
-                gameGrid[c, r] = " ";
+                grid[c, r] = " ";
             }
         }
     }
@@ -31,5 +32,29 @@ public class GameGrid
 // The main class, processing tree searching and evaluation
 public static class Bot
 { 
+    public static int maxDepth = 3;
+    public static GameGrid gameGrid;
+    public static Node root;
+    public static bool gameRunning = true;
+    private static string turn = "X";
+
+    static void Main()
+    {
+        // Creates an empty board with 7 columns, 6 rows
+        gameGrid = new GameGrid(7, 6);
+
+        while (gameRunning)
+        {
+            // Display the board
+
+            // Gets user input on their move
+            int col = Convert.ToInt16(Console.ReadLine());
+
+            // Make move on gameGrid, and checks if game ended
+
+            if (turn == "X") turn = "O";
+            else turn = "X";
+        }
+    }
 
 }
