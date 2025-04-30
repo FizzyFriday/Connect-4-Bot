bfb9a745778cd136d02a11622247d72f7178788e
Cameron Pollard-Grunill <CameronLP01@outlook.com>
2025-05-01 00:04:42 +0930

    Added BestMove starting method

--- Connect4 BotApp/Connect4 BotApp/Backend/Bot.cs
+++ Connect4 BotApp/Connect4 BotApp/Backend/Bot.cs
@@ -5,35 +5,35 @@ namespace Connect4_BotApp.Backend
     // Handles MCTS logic / tree searching. Communicates with other classes.
     internal static class Bot
     {
+        // PUBLIC METHODS
 
+        // Starts the bot - Accessed by GameController.cs
+        public static int BestMove(string[,] grid, string turn)
+        {
+            int bestCol = MCTSmanager();
+            return bestCol;
+        }
 
-
-
-
-
+        // Returns a list of allowed moves - Accessed by GameController.cs
         public static List<int[]> ValidMoves(string[,] grid)
         {
-            List<int[]> options = new();
-
-            for (int col = 0; col < grid.GetLength(0); col++)
-            {
-                int currentRow = grid.GetLength(1) - 1;
-                if (grid[col, currentRow] != " ") continue;
-
-                // Move down gradually, finding the lowest point that is empty as this is where the piece would go
-                while (grid[col, currentRow - 1] == " ")
-                {
-                    currentRow--;
-                    // Prevents checking negative indices
-                    if (currentRow == 0) break;
-                }
-
-                // Set the column and row of the move into an array
-                int[] moveOption = [col, currentRow];
-                options.Add(moveOption);
-            }
-            return options;
+            return GameBoard.ValidMoves(grid);
         }
+
+
+
+
+        // PRIVATE METHODS
+
+        // Manages and deals with results of MCTS
+        private static int MCTSmanager()
+        {
+            // Use original logic
+            return -1;
+        }
+
+
+        
     }
 }
 
--- Connect4 BotApp/Connect4 BotApp/Backend/GameBoard.cs
+++ Connect4 BotApp/Connect4 BotApp/Backend/GameBoard.cs
@@ -2,8 +2,30 @@
 
 namespace Connect4_BotApp
 {
-    internal class GameBoard
-    { 
-    
+    internal static class GameBoard
+    {
+        public static List<int[]> ValidMoves(string[,] grid)
+        {
+            List<int[]> options = new();
+
+            for (int col = 0; col < grid.GetLength(0); col++)
+            {
+                int currentRow = grid.GetLength(1) - 1;
+                if (grid[col, currentRow] != " ") continue;
+
+                // Move down gradually, finding the lowest point that is empty as this is where the piece would go
+                while (grid[col, currentRow - 1] == " ")
+                {
+                    currentRow--;
+                    // Prevents checking negative indices
+                    if (currentRow == 0) break;
+                }
+
+                // Set the column and row of the move into an array
+                int[] moveOption = [col, currentRow];
+                options.Add(moveOption);
+            }
+            return options;
+        }
     }
 }
